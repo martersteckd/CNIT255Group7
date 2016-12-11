cca12438407e52c76419068f96c301dcae0ad880
Gumbi1822 <mikaelacaron@gmail.com>
2016-12-11 14:29:16 -0500

    Created Name Class

---
+++ Snake/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Name
+    {
+        protected string firstName;
+        protected string lastName;
+        protected string midName;
+        protected string gamerTag;
+
+        protected string makeTag()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
